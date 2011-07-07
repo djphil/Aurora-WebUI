d1f8e55ad385767af974967a418f929285dd27bd
SignpostMarv <github@signpostmarv.name>
2011-07-07 23:52:56 +0100

    more short tag stuff

--- www/index.php
+++ www/index.php
@@ -595,7 +595,7 @@ $(document).ready(function(){
 <div id="container">
     <div id="header">
         <div id="headerimages">
-            <a href="<?php echo SYSURL ?>"><h1><? SYSNAME ?></h1></a>
+            <a href="<?php echo SYSURL ?>"><h1><?php echo SYSNAME; ?></h1></a>
         </div>
         <!-- <div id="gridstatus"><?php // include("sites/gridstatus.php"); ?></div> -->
         <div id="home_content_right"><?php include("sites/modules/slideshow.php"); ?></div>		
