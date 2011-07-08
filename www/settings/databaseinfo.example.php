9fd53c6b6bd2bae3e4539e9005c6db377fc2dd68
SignpostMarv <github@signpostmarv.name>
2011-07-08 21:11:43 +0100

    Updating databaseinfo.example.php to reflect initial PDO usage

--- www/settings/databaseinfo.example.php
+++ www/settings/databaseinfo.example.php
@@ -1,12 +1,12 @@
 <?php
 /*
- * Copyright (c) 2007, 2008 Contributors, http://opensimulator.org/
+ * Copyright (c) 2007, 2008, 2011 Contributors, http://opensimulator.org/
  * See CONTRIBUTORS for a full list of copyright holders.
  *
  * See LICENSE for the full licensing terms of this file.
  *
 */
-
+namespace{
 ##################### Database ########################
 define("C_DB_TYPE","mysql");
 // Your Hostname here:
@@ -14,7 +14,19 @@ define("C_DB_HOST","localhost");
 // Your Databasename here:
 define("C_DB_NAME","aurora");
 // Your Username from Database here:
-define("C_DB_USER","root");
+define("C_DB_USER","aurora");
 // Your Database Password here:
-define("C_DB_PASS","");
+define("C_DB_PASS","zfq4tMUbWPqvrbsD");
+
+}
+
+namespace Aurora\WebUI{
+	use Aurora\WebUI\PDO\helper as PDOH;
+
+##################### PDO #############################
+require('pdo.php');
+$PDODB = DB::i();
+$PDODB['Aurora']      = null; // replace with a call to PDOH::PDO()
+$PDODB['AuroraUsers'] = null; // replace with a call to PDOH::PDO()
+}
 ?>
