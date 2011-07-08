aa1e4f57b7a8521df5cb4c9aaccd21cfa6b1e310
SignpostMarv <github@signpostmarv.name>
2011-07-08 21:07:50 +0100

    Switching to PDO & OOP-based code

--- www/app/region/index.php
+++ www/app/region/index.php
@@ -1,39 +1,32 @@
-<?
+<?php
 include("../../settings/config.php");
 include("../../settings/databaseinfo.php");
 include("../../settings/mysql.php");
+include('../../settings/entity-types.php');
 include("../../settings/json.php");
 include("../../languages/translator.php");
 include("../../templates/templates.php");
 
 $DbLink = new DB;
-$query = "SELECT RegionName,LocX,LocY,OwnerUUID,Info FROM " . C_REGIONS_TBL . " where LocX='" . $_GET[x] . "' and LocY='" . $_GET[y] . "'";
-$query = "SELECT RegionName,LocX,LocY,OwnerUUID,Info FROM " . C_REGIONS_TBL . " where LocX='" . cleanQuery($_GET[x]) . "' and LocY='" . cleanQuery($_GET[y]) . "'";
-$DbLink->query($query);
-list($RegionName, $locX, $locY, $owner, $info) = $DbLink->next_record();
-$locX = $locX / 256;
-$locY = $locY / 256;
-$recieved = json_decode($info);
-$regionType = $recieved->{'regionType'};
-if ($regionType == '')
-    $regionType = 'Unknown';
-$DbLink->query("SELECT FirstName,LastName FROM " . C_USERS_TBL . " where PrincipalID='$owner'");
-$DbLink->query("SELECT FirstName,LastName FROM " . C_USERS_TBL . " where PrincipalID='" . cleanQuery($owner) . "'");
-list($firstN, $lastN) = $DbLink->next_record();
 
-$DbLink->query("SELECT RegionUUID, Info FROM " . C_REGIONS_TBL . " where locX='" . ($locX * 256) . "' and locY='" . ($locY * 256) . "'");
-list($UUID, $Info) = $DbLink->next_record();
-$recieved = json_decode($Info);
-$serverIP = $recieved->{'serverIP'};
-$serverHttpPort = $recieved->{'serverHttpPort'};
+$PDODB = Aurora\WebUI\DB::i();
+$region = Aurora\WebUI\RegionFromDB::get_by_LocX_LocY($PDODB['Aurora'], $_GET['x'], $_GET['y']);
+
+$locX = $region->LocX() / 256;
+$locY = $region->LocY() / 256;
+$regionInfo     = json_decode($region->Info());
+$regionType     = $regionInfo->regionType;
+$regionType     = $regionType == '' ? 'Unknown' : $regionType;
+$serverIP       = $regionInfo->{'serverIP'};
+$serverHttpPort = $regionInfo->{'serverHttpPort'};
+
+
+$regionOwner = Aurora\WebUI\UserFromDB::getRegionOwner($PDODB['AuroraUsers'], $region);
 
 $SERVER = "http://$serverIP:$serverHttpPort";
-$UUID = str_replace("-", "", $UUID);
+$UUID = str_replace("-", "", $region->RegionUUID());
 $source = $SERVER . "/index.php?method=regionImage" . $UUID . "";
-?>
 
-
-<?
 /* +++ PRINT NEIGHBORS +++ */
 // Array of 8 locations to search for
 $locarr['RegionName1'] = "(LocX='" . ($locX - 1) * 256 . "' and LocY='" . ($locY - 1) * 256 . "')";
@@ -46,6 +39,7 @@ $locarr['RegionName7'] = "(LocX='" . ($locX - 1) * 256 . "' and LocY='" . ($locY
 $locarr['RegionName8'] = "(LocX='" . $locX * 256 . "' and LocY='" . ($locY + 1) * 256 . "')";
 $locarr['RegionName9'] = "(LocX='" . ($locX + 1) * 256 . "' and LocY='" . ($locY + 1) * 256 . "')";
 
+/*
 $DbLink->query("SELECT RegionName,LocX,LocY FROM " . C_REGIONS_TBL . " where " . implode(" or ", $locarr));
 while (list($RegionNameX, $locX1, $locY1) = $DbLink->next_record()) {
 
@@ -86,6 +80,7 @@ while (list($RegionNameX, $locX1, $locY1) = $DbLink->next_record()) {
     }
     ${"RegionName" . $regN} = "<a href='?x=" . $locX1 . "&y=" . $locY1 . "'>" . $RegionNameX . "</a>";
 }
+*/
 
   $DbLink->query("SELECT id,
                          displayTopPanelSlider, 
@@ -135,7 +130,7 @@ while (list($RegionNameX, $locX1, $locY1) = $DbLink->next_record()) {
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <link rel="stylesheet" href="<?php echo SYSURL; ?><?php echo $template_css ?>" type="text/css" />
-  <link rel="icon" href="<?php echo SYSURL; ?><?= $favicon_image ?>" />
+  <link rel="icon" href="<?php echo SYSURL, $favicon_image; ?>" />
   <title><?php echo SYSNAME; ?>: <?php echo $webui_region_information; ?></title>
 
 <?php if($displayRoundedCorner)  { ?>
@@ -153,55 +148,55 @@ while (list($RegionNameX, $locX1, $locY1) = $DbLink->next_record()) {
 <body class="webui">
 <div id="container_popup">
 <div id="content_popup">
-  <h2><?php echo SYSNAME; ?>: <?php echo $webui_region_information; ?></h2>
+  <h2><?php echo SYSNAME, ': ',$webui_region_information; ?></h2>
   
   <div id="regioninfo">
-  <!--  <div id="info"><p><? // echo $webui_regioninfo ?></p></div> -->
-  <!--  <h2><? // echo $webui_region_information; ?>:</h2> -->
 
     <hr>
+<!--
     <div id="regionMap">
       <table cellpadding="0" cellspacing="4">
         <tr>
-          <td <?php echo ($RegionName1 ? ">" . $RegionName1 : "class='nosim'>") ?></td>
-          <td <?php echo ($RegionName2 ? ">" . $RegionName2 : "class='nosim'>") ?></td>
-          <td <?php echo ($RegionName3 ? ">" . $RegionName3 : "class='nosim'>") ?></td>
+          <td <?php # echo ($RegionName1 ? ">" . $RegionName1 : "class='nosim'>") ?></td>
+          <td <?php # echo ($RegionName2 ? ">" . $RegionName2 : "class='nosim'>") ?></td>
+          <td <?php # echo ($RegionName3 ? ">" . $RegionName3 : "class='nosim'>") ?></td>
         </tr>
       
         <tr>
-          <td <?php echo ($RegionName4 ? ">" . $RegionName4 : "class='nosim'>") ?></td>
-          <td class='thissim'><?= $RegionName ?></td>
-          <td <?php echo ($RegionName6 ? ">" . $RegionName6 : "class='nosim'>") ?></td>
+          <td <?php # echo ($RegionName4 ? ">" . $RegionName4 : "class='nosim'>") ?></td>
+          <td class='thissim'><?php # echo $region->RegionName(); ?></td>
+          <td <?php # echo ($RegionName6 ? ">" . $RegionName6 : "class='nosim'>") ?></td>
         </tr>
       
         <tr>
-          <td <?php echo ($RegionName7 ? ">" . $RegionName7 : "class='nosim'>") ?></td>
-          <td <?php echo ($RegionName8 ? ">" . $RegionName8 : "class='nosim'>") ?></td>
-          <td <?php echo ($RegionName9 ? ">" . $RegionName9 : "class='nosim'>") ?></td>
+          <td <?php # echo ($RegionName7 ? ">" . $RegionName7 : "class='nosim'>") ?></td>
+          <td <?php # echo ($RegionName8 ? ">" . $RegionName8 : "class='nosim'>") ?></td>
+          <td <?php # echo ($RegionName9 ? ">" . $RegionName9 : "class='nosim'>") ?></td>
         </tr>
       </table>
     </div>
+-->
 
     <div id="region_picture">
-      <img src="<?php echo $source; ?>" alt="<?= $RegionName ?>" title="<?= $RegionName ?>" />
+      <img src="<?php echo $source; ?>" alt="<?php echo $region->RegionName(); ?>" title="<?php echo $region->RegionName(); ?>" />
     </div>
 
     <div id="regiondetails">
       <table>
         <tr>
-          <td><?php echo $webui_region_name; ?>: <?= $RegionName ?></td>
+          <td><?php echo $webui_region_name, ': ', $region->RegionName(); ?></td>
         </tr>
       
         <tr>
-          <td><?php echo $webui_region_type; ?>: <?= $regionType ?></td>
+          <td><?php echo $webui_region_type, ': ', $regionType; ?></td>
         </tr>
       
         <tr>
-          <td><?php echo $webui_location; ?> X: <?= $locX ?> Y: <?= $locY ?></td>
+          <td><?php echo $webui_location, ' X: ', $locX, ' Y: ', $locY; ?></td>
         </tr>
       
         <tr>
-          <td><?php echo $webui_owner; ?>: <a href="<?php echo SYSURL; ?>app/agent/?name=<?= $firstN ?> <?= $lastN ?>"><?= $firstN ?> <?= $lastN ?></a></td>
+          <td><?php echo $webui_owner; ?>: <a href="<?php echo SYSURL; ?>app/agent/?name=<?php echo urlencode($regionOwner->FirstName() . ' ' . $regionOwner->LastName()); ?>"><?php echo htmlentities($regionOwner->FirstName() . ' ' . $regionOwner->LastName()); ?></a></td>
         </tr>
       </table>
     </div>
